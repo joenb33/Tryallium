af6bbd090f25d753e443487a68624dcbcee0644c
joenb33 <joenb33@gmail.com>
2023-02-22 22:59:57 +0100

    Adding stuff to the home

--- pages/home.php
+++ pages/home.php
@@ -6,14 +6,15 @@ $pageAuthor = "Joen";
 $pageName = "home";
 include("../includes/header.php");
 ?>
-
-<div class="background-image"></div>
-	<div class="content">
-		<h1>Welcome to Tryallium</h1>
-		<p>Trying new things, one step at a time</p>
-	</div>
+	<div class="background-image-section"></div>
+	<section class="main-content">
+		<div class="container">
+			<h1>Welcome to Tryallium</h1>
+			<p>Trying new things, one step at a time</p>
+		</div>
+	</section>
 	
-	<section id="content">
+	<section class="about-content">
 		<div class="container">
 			<h2>About Tryallium</h2>
 			<p>Tryallium is a website where you can find a collection of fun and interesting projects that ChatGPT has made. Whether you're looking to learn something new or just want to try something different, we've got you covered. Explore our site and see what you can discover!</p>
